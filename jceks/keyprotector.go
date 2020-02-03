6b7b1a706033243f27be511517107c8d016b18af
Gabor Tanz <izolight@gmail.com>
2020-02-03 13:59:11 +0100

    put keystream generation in separate function

--- jceks/keyprotector.go
+++ jceks/keyprotector.go
@@ -5,42 +5,31 @@ import (
 	"crypto/sha1"
 	"errors"
 	"fmt"
+	"hash"
 )
 
-const saltLen = 20
-
 func recoverKeyProtector(data, password []byte) ([]byte, error) {
-	md := sha1.New()
 	passwordBytes := make([]byte, 0, len(password)*2)
 	for _, b := range password {
 		passwordBytes = append(passwordBytes, 0, b)
 	}
+	// salt is the first 20 bytes of the encrypted key
+	const saltLen = sha1.Size
 	salt := make([]byte, saltLen)
 	copy(salt, data)
-	encryptedKeyLen := len(data) - saltLen - md.Size()
-	numRounds := encryptedKeyLen / md.Size()
-	if encryptedKeyLen%md.Size() != 0 {
-		numRounds++
-	}
+
+	md := sha1.New()
+	encryptedKeyLen := len(data) - saltLen - sha1.Size
 	encryptedKey := make([]byte, encryptedKeyLen)
 	copy(encryptedKey, data[saltLen:])
-	xorKey := make([]byte, encryptedKeyLen)
-	digest := salt
-	for i, xorOffset := 0, 0; i < numRounds; i++ {
-		if _, err := md.Write(passwordBytes); err != nil {
-			return nil, fmt.Errorf("update digest with password on %d round: %w", i, err)
-		}
-		if _, err := md.Write(digest); err != nil {
-			return nil, fmt.Errorf("update digest with digest from previous round on %d round: %w", i, err)
-		}
-		digest = md.Sum(nil)
-		md.Reset()
-		copy(xorKey[xorOffset:], digest)
-		xorOffset += md.Size()
+
+	keystream, err := generateKeystream(passwordBytes, salt, encryptedKeyLen, md)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate keystream: %w", err)
 	}
 	plainKey := make([]byte, encryptedKeyLen)
 	for i := 0; i < len(plainKey); i++ {
-		plainKey[i] = encryptedKey[i] ^ xorKey[i]
+		plainKey[i] = encryptedKey[i] ^ keystream[i]
 	}
 
 	if _, err := md.Write(passwordBytes); err != nil {
@@ -49,7 +38,7 @@ func recoverKeyProtector(data, password []byte) ([]byte, error) {
 	if _, err := md.Write(plainKey); err != nil {
 		return nil, fmt.Errorf("update digest with plain key: %w", err)
 	}
-	digest = md.Sum(nil)
+	digest := md.Sum(nil)
 	md.Reset()
 
 	digestOffset := saltLen + encryptedKeyLen
@@ -57,4 +46,30 @@ func recoverKeyProtector(data, password []byte) ([]byte, error) {
 		return nil, errors.New("got invalid digest")
 	}
 	return plainKey, nil
+}
+
+// generate the keystream which is sha1(password + salt),
+// with the output being the salt for the next round until
+// the length matches the encrypted key.
+func generateKeystream(password, salt []byte, length int, digestAlgorithm hash.Hash) ([]byte, error) {
+	keystream := make([]byte, length)
+	rounds := length / digestAlgorithm.Size()
+	if length%digestAlgorithm.Size() != 0 {
+		rounds++
+	}
+
+	for i, xorOffset := 0, 0; i < rounds; i++ {
+		if _, err := digestAlgorithm.Write(password); err != nil {
+			return nil, fmt.Errorf("update digest with password on %d round: %w", i, err)
+		}
+		if _, err := digestAlgorithm.Write(salt); err != nil {
+			return nil, fmt.Errorf("update digest with digest from previous round on %d round: %w", i, err)
+		}
+		salt = digestAlgorithm.Sum(nil)
+		digestAlgorithm.Reset()
+		copy(keystream[xorOffset:], salt)
+		xorOffset += digestAlgorithm.Size()
+	}
+
+	return keystream, nil
 }
